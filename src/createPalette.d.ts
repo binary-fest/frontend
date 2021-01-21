917cd188a077ac3bd0670a8456a9332b4c2c9d3d
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-21 19:54:30 +0700

    feat(theme): add extend theme : gradient

--- src/createPalette.d.ts
+++ src/createPalette.d.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as createPalette from '@material-ui/core/styles/createPalette';
 
 declare module "@material-ui/core/styles/createPalette" {
   interface Palette {
-    gradient?: Palette['primary'];
+    gradient: PaletteColor;
   }
   interface PaletteOptions {
-    gradient?: PaletteOptions['primary'];
+    gradient: PaletteOptions['primary'];
   }
 }
