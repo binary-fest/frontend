ba78381351e55394733d1bb7145bde511516e696
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-21 17:26:50 +0700

    feat(navigation): create responsive between links and hamburger icon

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,19 +1,59 @@
-import { Container, Grid, makeStyles } from '@material-ui/core'
+import { Container, Grid, makeStyles, Typography } from '@material-ui/core'
 import React, { ReactElement, useState } from 'react'
 import NavigationResponsive from './NavigationResponsive'
 
-const useStyles = makeStyles(({spacing}) => ({
+const useStyles = makeStyles(({spacing, breakpoints}) => ({
   root: {
     top: spacing(2),
     position: 'relative'
   },
   hamburgerIcon: {
-    cursor: 'pointer'
+    cursor: 'pointer',
+    [breakpoints.up('sm')]: {
+      display: 'none'
+    }
+  },
+  listLink: {
+    display: 'none',
+    "& *": {
+      color: 'white',
+      textDecoration: 'none'
+    },
+    width: '378px',
+    [breakpoints.up('sm')]: {
+      display: 'flex',
+      justifyContent: 'space-between'
+    }
+  },
+  containerLinkResponsive: {
+    [breakpoints.up('sm')]: {
+      position: 'absolute',
+      left: "50%",
+      transform: 'translateX(-50%)'
+    }
   }
 }))
 
 export default function Navigation(): ReactElement {
   const [isNavigationResponsiveShow, setIsNavigationResponsiveShow] = useState(false)
+
+  const listNavigationLink = [{
+    id: 1,
+    name: 'Home',
+    href: '/'
+  }, {
+    id: 2,
+    name: 'About',
+    href: '/'
+  }, {
+    id: 3,
+    name: 'Kompetisi',
+    href: '/'
+  }, {
+    id: 4,
+    name: 'Expo',
+    href: '/'
+  }]
 
   const toggleNavigationResponsive = () => setIsNavigationResponsiveShow(!isNavigationResponsiveShow)
 
@@ -25,13 +65,22 @@ export default function Navigation(): ReactElement {
         <Grid item>
           <img src="/binary-fest-logo-mobile.svg" alt="Binary Fest"/>
         </Grid>
-        <Grid item>
+        <Grid item className={classes.containerLinkResponsive}>
           <img
             className={classes.hamburgerIcon}
             src="/hamburger-icon.svg"
             alt="Binary Fest"
             onClick={toggleNavigationResponsive}
           />
+          <div className={classes.listLink}>
+            {listNavigationLink.map(link => {
+              return (
+                <a key={link.id} href={link.href}>
+                  <Typography>{link.name}</Typography>
+                </a>
+              )
+            })}
+          </div>
         </Grid>
       </Grid>
       { isNavigationResponsiveShow && <NavigationResponsive toggleHandler={setIsNavigationResponsiveShow}/> }
