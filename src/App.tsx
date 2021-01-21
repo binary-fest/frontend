f0e864ea6fccabb1f079b1a5ead4ffecb7a1ebc9
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-21 19:55:36 +0700

    feat(app): adding route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { makeStyles, ThemeProvider, Typography } from '@material-ui/core';
+import { Container, makeStyles } from '@material-ui/core';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
 import Navigation from './components/Navigation';
-import theme from './theme';
+import { Route, Switch } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Competition from './pages/Competition';
+import Expo from './pages/Expo';
 
 const useStyles = makeStyles(({palette}) => ({
   root: {
     minHeight: '100vh',
-    background: palette.gradient?.main
+    background: palette.gradient.main
   }
 }))
 
@@ -15,14 +19,27 @@ function App() {
   const classes = useStyles()
 
   return (
-    <ThemeProvider theme={theme}>
-      <RecoilRoot>
-        <div className={classes.root} style={{backgroundColor: 'black'}}>
-          <Navigation />
-          <Typography>Hello world !</Typography>
-        </div>
-      </RecoilRoot>
-    </ThemeProvider>
+    <RecoilRoot>
+      <div className={classes.root}>
+        <Navigation />
+        <Container>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/competition">
+              <Competition />
+            </Route>
+            <Route path="/expo">
+              <Expo />
+            </Route>
+          </Switch>
+        </Container>
+      </div>
+    </RecoilRoot>
   );
 }
 
