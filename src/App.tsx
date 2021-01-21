afd9c35ad0c099043ddb280b184d191464bddfc3
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-21 17:55:45 +0700

    feat(app): adding background and min-height

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { ThemeProvider, Typography } from '@material-ui/core';
+import { makeStyles, ThemeProvider, Typography } from '@material-ui/core';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
 import Navigation from './components/Navigation';
 import theme from './theme';
 
+const useStyles = makeStyles(() => ({
+  root: {
+    minHeight: '100vh',
+    background: "radial-gradient(312.96% 312.96% at 109.41% -262.96%, #780031 13.16%, #100E17 100%)"
+  }
+}))
+
 function App() {
+  const classes = useStyles()
+
   return (
     <ThemeProvider theme={theme}>
       <RecoilRoot>
-        <div className="App" style={{backgroundColor: 'black'}}>
+        <div className={classes.root} style={{backgroundColor: 'black'}}>
           <Navigation />
           <Typography>Hello world !</Typography>
         </div>
