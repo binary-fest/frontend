126e3ba6bffc2c8de79fe15d03c030096bdcf557
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-21 16:19:07 +0700

    feat(navigation): adding navigation content for mobil

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider, Typography } from '@material-ui/core';
 import React from 'react';
+import Navigation from './components/Navigation';
 import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
+      <div className="App" style={{backgroundColor: 'black'}}>
+        <Navigation />
         <Typography>Hello world !</Typography>
       </div>
     </ThemeProvider>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,29 @@
+import { Container, Grid, makeStyles } from '@material-ui/core'
 import React, { ReactElement } from 'react'
 
+const useStyles = makeStyles(({spacing}) => ({
+  root: {
+    top: spacing(2),
+    position: 'relative'
+  },
+  hamburgerIcon: {
+    cursor: 'pointer'
+  }
+}))
+
 export default function Navigation(): ReactElement {
+  const classes = useStyles()
+
   return (
-    <div>
-      
-    </div>
+    <Container className={classes.root}>
+      <Grid container justify="space-between" alignItems="center">
+        <Grid item>
+          <img src="/binary-fest-logo-mobile.svg" alt="Binary Fest"/>
+        </Grid>
+        <Grid item>
+          <img className={classes.hamburgerIcon} src="/hamburger-icon.svg" alt="Binary Fest"/>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
