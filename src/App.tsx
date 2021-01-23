16a26038a76739345801d49852d3738d3a695f81
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-23 07:02:14 +0700

    refactor(layout): separating file layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
-import { Container, makeStyles } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
-import Navigation from './components/Navigation';
-import { Route, Switch } from 'react-router-dom'
-import Home from './pages/Home';
-import About from './pages/About';
-import Competition from './pages/Competition';
-import Expo from './pages/Expo';
-import FooterPage from './components/FooterPage';
-import FixedSocialMedia from './components/FixedSocialMedia';
+import StaticPageLayout from './layout/StaticPage.layout';
 
 const useStyles = makeStyles(({palette, breakpoints}) => ({
   root: {
     minHeight: '100vh',
     background: palette.gradient.main
-  },
-  decoration1: {
-    display: 'none',
-    position: 'absolute',
-    top: '236px',
-    left: '60px',
-    [breakpoints.up('sm')]: {
-      display: 'flex'
-    },
-    [breakpoints.up('md')]: {
-      left: '96px'
-    }
   }
 }))
 
@@ -35,26 +16,7 @@ function App() {
   return (
     <RecoilRoot>
       <div className={classes.root}>
-        <Navigation />
-        <img className={classes.decoration1} src="/decoration-1.svg" alt="decoration"/>
-        <FixedSocialMedia />
-        <Container>
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/competition">
-              <Competition />
-            </Route>
-            <Route path="/expo">
-              <Expo />
-            </Route>
-          </Switch>
-        </Container>
-        <FooterPage />
+        <StaticPageLayout />
       </div>
     </RecoilRoot>
   );
