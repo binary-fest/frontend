61779222a0ac1ab14afcd0389bdffb9e4dc9379a
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-23 11:57:46 +0700

    feat(store/date): update countdown date

--- src/store/date.ts
+++ src/store/date.ts
@@ -2,7 +2,7 @@ import { atom } from 'recoil';
 
 const countdownEventDateAtom = atom<Date>({
   key: 'countdownEventDate',
-  default: new Date("2021-02-01:0:0:0")
+  default: new Date("2021-03-01:0:0:0")
 })
 
 export { countdownEventDateAtom }
