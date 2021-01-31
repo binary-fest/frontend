39208ca1a23cb3b7792569a366f4262f235f92bf
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-31 23:30:13 +0700

    fix(public/index.html): fix starter title tag

--- src/hooks/useTitlePage.ts
+++ src/hooks/useTitlePage.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const useTitlePage = (initialTitle: string) => {
-  const [title, setTitle] = useState(initialTitle || "React App")
+  const [title, setTitle] = useState(initialTitle || "BinaryFest 2021")
   document.title = title
 
   return [title, setTitle]
