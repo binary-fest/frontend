1c0e899e1e07cd3c4de2d8aa507aab4c0f359ce5
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-31 23:26:50 +0700

    feat(package): init axios

---
+++ src/utils/http.ts
@@ -0,0 +1,9 @@
+import axios, { AxiosRequestConfig } from 'axios'
+
+const httpConfig: AxiosRequestConfig = {
+  baseURL: 'http://localhost:9000/api'
+}
+
+const http = axios.create(httpConfig)
+
+export { http }
