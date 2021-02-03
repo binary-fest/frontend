78fbfc19d45a3b67f39ab706b4a8bf49668d9fa2
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-02-03 21:06:10 +0700

    feat(store/members): adding global state to handle members

---
+++ src/store/members.ts
@@ -0,0 +1,43 @@
+import { atom } from 'recoil'
+
+interface Member {
+  name: string
+  nim: string
+  email: string
+  phone: string
+  gender: "pria" | "wanita"
+  isAdmin: boolean
+}
+
+const membersAtom = atom<Member[]>({
+  key: 'membersAtom',
+  default: [
+    {
+      "name": "Beatrice",
+      "nim": "sint",
+      "email": "beatriceglenn@squish.com",
+      "phone": "(855) 522-2859",
+      "gender": "wanita",
+      "isAdmin": false
+    },
+    {
+      "name": "Frost",
+      "nim": "occaecat",
+      "email": "frostglenn@squish.com",
+      "phone": "(814) 495-3603",
+      "gender": "pria",
+      "isAdmin": true
+    },
+    {
+      "name": "andi",
+      "nim": "occaecat",
+      "email": "frostglenn@squish.com",
+      "phone": "(814) 495-3603",
+      "gender": "pria",
+      "isAdmin": false
+    }
+  ]
+})
+
+export default membersAtom
+export type { Member }
