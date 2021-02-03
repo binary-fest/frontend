014d472c9a7415ac00d71ad9ff4bc485df451360
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-02-03 16:41:46 +0700

    feat(register-page): adding select competition

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,6 @@
-import { Container, FormControl, Grid, makeStyles } from '@material-ui/core'
-import React, { ReactElement } from 'react'
-import { WhiteInput, WhiteInputLabel, WhiteTypography } from '../theme/extends'
+import { Button, Container, FormControl, Grid, makeStyles, Typography } from '@material-ui/core'
+import React, { ReactElement, useState } from 'react'
+import { GradientButton, WhiteInput, WhiteInputLabel, WhiteTypography } from '../theme/extends'
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -11,10 +11,35 @@ const useStyles = makeStyles(() => ({
     '& > div': {
       marginTop: '29px'
     }
+  },
+  competitionType: {
+    marginTop: '3rem',
+    '& button': {
+      marginRight: '16px',
+      marginTop: '29px'
+    }
   }
 }))
 
 export default function Register(): ReactElement {
+  const [competitions, setCompetitions] = useState([
+    {
+      id: 'IOT',
+      name: 'Internet of Things',
+      isSelected: true,
+    },
+    {
+      id: 'UIUX',
+      name: 'UI / UX',
+      isSelected: false,
+    }
+  ])
+
+  const selectCompetition = (id: string) => {
+    setCompetitions(
+      competitions.map((comp) => ({ ...comp, isSelected: comp.id === id }))
+    )
+  }
 
   const classes = useStyles()
 
@@ -40,6 +65,32 @@ export default function Register(): ReactElement {
               <WhiteInputLabel htmlFor="input-judul">Judul</WhiteInputLabel>
               <WhiteInput id="input-judul" fullWidth />
             </FormControl>
+          </div>
+        </Grid>
+      
+        <Grid item xs={12} className={classes.competitionType}>
+          <div>
+            <WhiteTypography variant="h3">Jenis Perlombaan</WhiteTypography>
+            {competitions.map((competition) => (
+              competition.isSelected ? (
+                <GradientButton
+                  key={competition.id}
+                  style={{ padding: '16px' }}
+                  onClick={() => selectCompetition(competition.id)}
+                >
+                  <WhiteTypography>{competition.name}</WhiteTypography>
+                </GradientButton>
+              ) : (
+                <Button
+                  variant="contained"
+                  key={competition.id}
+                  style={{ padding: '16px' }}
+                  onClick={() => selectCompetition(competition.id)}
+                >
+                  <Typography>{competition.name}</Typography>
+                </Button>
+              )
+            ))}
           </div>
         </Grid>
       </Grid>
