5b5220fd4ac7d96bcbc719abf429f0b321ad5241
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-02-06 00:19:42 +0700

    feat(register-page): create init formik for team information

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,6 +3,14 @@ import React, { ReactElement, useState } from 'react'
 import ListMember from '../components/ListMember'
 import { GradientButton, WhiteInput, WhiteInputLabel, WhiteTypography } from '../theme/extends'
 import MemberModalPopup from '../components/MemberModalPopup'
+import { useFormik } from 'formik'
+import { CompetitionType, Team } from '../@types/Team'
+
+interface CompetitionState {
+  id: CompetitionType,
+  name: string,
+  isSelected: boolean
+}
 
 const useStyles = makeStyles(({breakpoints}) => ({
   root: {
@@ -34,15 +42,24 @@ const useStyles = makeStyles(({breakpoints}) => ({
   }
 }))
 
+const initialValueFormik: Team = {
+  name: '',
+  email: '',
+  institute: '',
+  title: '',
+  url_files: '',
+  competition_type: 'iot'
+}
+
 export default function Register(): ReactElement {
-  const [competitions, setCompetitions] = useState([
+  const [competitions, setCompetitions] = useState<CompetitionState[]>([
     {
-      id: 'IOT',
+      id: 'iot',
       name: 'Internet of Things',
       isSelected: true,
     },
     {
-      id: 'UIUX',
+      id: 'uiux',
       name: 'UI / UX',
       isSelected: false,
     }
@@ -55,71 +72,117 @@ export default function Register(): ReactElement {
     )
   }
 
+  const formik = useFormik({
+    initialValues: { ...initialValueFormik },
+    onSubmit: (values) => {
+      const [selectedCompetition] = competitions.filter(comp => comp.isSelected)
+
+      const teamData: Team = {
+        name: values.name,
+        email: values.email,
+        institute: values.institute,
+        title: values.title,
+        competition_type: selectedCompetition.id,
+        url_files: values.url_files
+      }
+      alert(JSON.stringify(teamData, undefined, 2))
+    }
+  })
+
   const classes = useStyles()
 
   return (
     <>
     <MemberModalPopup />
-    <Container className={classes.root}>
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={6} lg={5}>
-          <WhiteTypography variant="h3">Pendaftaran Team</WhiteTypography>
-          <div className={classes.teamForm}>
-            <FormControl fullWidth>
-              <WhiteInputLabel htmlFor="input-name-team">Nama Team</WhiteInputLabel>
-              <WhiteInput id="input-name-team" fullWidth />
-            </FormControl>
-            <FormControl fullWidth>
-              <WhiteInputLabel htmlFor="input-email-team">Email ( Perwakilan )</WhiteInputLabel>
-              <WhiteInput id="input-email-team" type="email" fullWidth />
-            </FormControl>
-            <FormControl fullWidth>
-              <WhiteInputLabel htmlFor="input-instansi-team">Instansi</WhiteInputLabel>
-              <WhiteInput id="input-instansi-team" fullWidth />
-            </FormControl>
-            <FormControl fullWidth>
-              <WhiteInputLabel htmlFor="input-judul">Judul</WhiteInputLabel>
-              <WhiteInput id="input-judul" fullWidth />
-            </FormControl>
-          </div>
-        </Grid>
-      
-        <Grid item xs={12} md={6} lg={7} className={classes.competitionType}>
-          <div>
-            <WhiteTypography variant="h3">Jenis Perlombaan</WhiteTypography>
-            {competitions.map((competition) => (
-              competition.isSelected ? (
-                <GradientButton
-                  key={competition.id}
-                  style={{ padding: '16px' }}
-                  data-competition={competition.id}
-                  onClick={selectCompetitionHandler}
-                >
-                  <WhiteTypography>{competition.name}</WhiteTypography>
-                </GradientButton>
-              ) : (
-                <Button
-                  variant="contained"
-                  key={competition.id}
-                  style={{ padding: '16px' }}
-                  data-competition={competition.id}
-                  onClick={selectCompetitionHandler}
-                >
-                  <Typography>{competition.name}</Typography>
-                </Button>
-              )
-            ))}
-          </div>
-          <div className="form-input-link">
-            <WhiteTypography variant="h3">Link Berkas</WhiteTypography>
-            <FormControl fullWidth>
-              <WhiteInputLabel htmlFor="input-judul">Link</WhiteInputLabel>
-              <WhiteInput id="input-judul" fullWidth />
-            </FormControl>
-          </div>
-        </Grid>
-      </Grid>
-      <ListMember />
+      <Container className={classes.root}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            formik.handleSubmit()
+          }}
+        >
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={6} lg={5}>
+              <WhiteTypography variant="h3">Pendaftaran Team</WhiteTypography>
+              <div className={classes.teamForm}>
+                <FormControl fullWidth>
+                  <WhiteInputLabel htmlFor="input-name-team">Nama Team</WhiteInputLabel>
+                  <WhiteInput
+                    id="input-name-team"
+                    fullWidth
+                    onChange={formik.handleChange}
+                    name="name"
+                  />
+                </FormControl>
+                <FormControl fullWidth>
+                  <WhiteInputLabel htmlFor="input-email-team">Email ( Perwakilan )</WhiteInputLabel>
+                  <WhiteInput
+                    id="input-email-team"
+                    type="email"
+                    fullWidth
+                    onChange={formik.handleChange}
+                    name="email"
+                  />
+                </FormControl>
+                <FormControl fullWidth>
+                  <WhiteInputLabel htmlFor="input-instansi-team">Instansi</WhiteInputLabel>
+                  <WhiteInput
+                    id="input-instansi-team"
+                    fullWidth
+                    onChange={formik.handleChange}
+                    name="institute"
+                  />
+                </FormControl>
+                <FormControl fullWidth>
+                  <WhiteInputLabel htmlFor="input-judul">Judul</WhiteInputLabel>
+                  <WhiteInput
+                    id="input-judul"
+                    fullWidth
+                    onChange={formik.handleChange}
+                    name="title"
+                  />
+                </FormControl>
+              </div>
+            </Grid>
+          
+            <Grid item xs={12} md={6} lg={7} className={classes.competitionType}>
+              <div>
+                <WhiteTypography variant="h3">Jenis Perlombaan</WhiteTypography>
+                {competitions.map((competition) => (
+                  competition.isSelected ? (
+                    <GradientButton
+                      key={competition.id}
+                      style={{ padding: '16px' }}
+                      data-competition={competition.id}
+                      onClick={selectCompetitionHandler}
+                    >
+                      <WhiteTypography>{competition.name}</WhiteTypography>
+                    </GradientButton>
+                  ) : (
+                    <Button
+                      variant="contained"
+                      key={competition.id}
+                      style={{ padding: '16px' }}
+                      data-competition={competition.id}
+                      onClick={selectCompetitionHandler}
+                    >
+                      <Typography>{competition.name}</Typography>
+                    </Button>
+                  )
+                ))}
+              </div>
+              <div className="form-input-link">
+                <WhiteTypography variant="h3">Link Berkas</WhiteTypography>
+                <FormControl fullWidth>
+                  <WhiteInputLabel htmlFor="input-link">Link</WhiteInputLabel>
+                  <WhiteInput id="input-link" fullWidth onChange={formik.handleChange} name="url_files"/>
+                </FormControl>
+              </div>
+            </Grid>
+          </Grid>
+          <ListMember />
+          <GradientButton type="submit">Register</GradientButton>
+      </form>
     </Container>
     </>
   )
