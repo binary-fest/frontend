f55f050fa6110d333f03cd94e70d9f9d5676e758
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-02-11 10:51:49 +0700

    feat(webinar-page): adding title h1

--- src/pages/Webinar.tsx
+++ src/pages/Webinar.tsx
@@ -1,9 +1,21 @@
 import React, { ReactElement } from 'react'
+import { GradientTypography, WhiteTypography } from '../theme/extends'
+import { StyledWebinarContainer } from '../theme/pages/Webinar'
 
 export default function Webinar(): ReactElement {
   return (
-    <div>
-      <h1>Webinar Page</h1>
-    </div>
+    <>
+      <StyledWebinarContainer>
+        <GradientTypography
+          variant="h3" 
+          style={{ fontSize: '36px' }} 
+          align="center"
+        >Webinar Nasional</GradientTypography>
+        <WhiteTypography
+          style={{ marginTop: '16px' }}
+          align="center"
+        >Peran IoT dan Cyber Security di Era Revolusi 4.0</WhiteTypography>
+      </StyledWebinarContainer>
+    </>
   )
 }
---
+++ src/theme/pages/Webinar.ts
@@ -0,0 +1,11 @@
+import { Container, withStyles } from '@material-ui/core'
+
+const StyledWebinarContainer = withStyles({
+  root: {
+    paddingTop: '389px'
+  }
+})(Container)
+
+export {
+  StyledWebinarContainer
+}
