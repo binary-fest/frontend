781389cdfe01c78da5b55f3794a8672f410c4a0a
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-02-17 20:19:12 +0700

    feat(utils/validate): create validation is url for external url

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -4,6 +4,11 @@ interface Validate {
 }
 
 const isValidEmail = (email: string) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)
+const isExternalUrl = (url: string) => {
+  const isHttp = url.substr(0, 7).toLocaleLowerCase() === "http://" 
+  const isHttps = url.substr(0, 8).toLocaleLowerCase() === "https://" 
+  return isHttp || isHttps
+}
 
 const validateEmail = (email: string): Validate => {
   const validating = isValidEmail(email)
@@ -16,5 +21,6 @@ const validateEmail = (email: string): Validate => {
 
 export {
   isValidEmail,
-  validateEmail
+  validateEmail,
+  isExternalUrl
 }
