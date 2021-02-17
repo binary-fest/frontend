ae9ee1befb206fe06e64cbbd41a6c3fc3b0a2120
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-02-17 13:31:55 +0700

    fix(pages/home): remove top position on Highlight

--- src/pages/Home/Home.styled.ts
+++ src/pages/Home/Home.styled.ts
@@ -1,16 +1,12 @@
 import Styled from 'styled-components'
-import theme from '../../theme'
 import { GradientTypography } from '../../theme/extends'
 
 const Highlight = Styled.img`
   position: absolute;
-  top: 280px;
   max-width: 446.5px;
   width: 100%;
   z-index: 1;
-  ${theme.breakpoints.up('sm')} {
-    top: 340px;
-  }
+  top: 360px;
 `
 
 const Arrows = Styled.div`
