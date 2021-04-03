e06891aca17771be96194e8297840c86976f3325
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-04-03 20:55:31 +0700

    feat(components): show lecturers on ListLecturer

--- src/components/ListLecturer.tsx
+++ src/components/ListLecturer.tsx
@@ -1,15 +1,21 @@
-import React, { ReactElement } from 'react'
-import { useRecoilValue } from 'recoil'
-import { lecturerState } from '../store/lecturer'
+import React, { ReactElement, useEffect, useState } from 'react'
+import { LecturerState } from '../@types/Lecturer'
 import { StyledListLecturer } from '../theme/components/ListLecturer'
 import LecturerCard from './LecturerCard'
+import { fetchWebinarLecturer } from '../http/webinar'
 
 export default function ListLecturer(): ReactElement {
-  const lecturers = useRecoilValue(lecturerState)
+  const [lecturers, setLecturers] = useState<LecturerState[]>([])
+
+  useEffect(() => {
+    fetchWebinarLecturer().then(data => setLecturers(data))
+  }, [])
 
   return (
     <StyledListLecturer>
-      {lecturers.map(lecturer => (<LecturerCard lecturer={lecturer} key={lecturer.id} data-aos="fade-up"/>))}
+      {lecturers.map(lecturer => (
+        <LecturerCard lecturer={lecturer} key={lecturer.id} data-aos="fade-up"/>
+      ))}
     </StyledListLecturer>
   )
 }
