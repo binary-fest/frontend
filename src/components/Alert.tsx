88376b1667d0bc222b1b4c6d792f82891bd17fde
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-04-16 05:06:26 +0700

    refactor(alert): using local styled

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,18 +1,10 @@
-import { Typography } from '@material-ui/core'
+import { Typography, withStyles } from '@material-ui/core'
 import React, { ReactElement } from 'react'
 import { CSSTransition } from 'react-transition-group'
-import {
-  StyledAlert,
-  StyledAlertButtonClose,
-  StyledAlertCloseZone,
-  StyledAlertIconError,
-  StyledAlertIconSpinner,
-  StyledAlertIconSuccess,
-  StyledAlertRoot,
-  StyledAlertTitle
-} from '../theme/components/Alert'
 import { WhiteTypography } from '../theme/extends'
 import Backdrop from './Backdrop'
+import styled from 'styled-components';
+import { GradientButton } from '../theme/extends';
 
 export interface AlertStatus {
   isShow: boolean
@@ -31,16 +23,114 @@ interface AlertContentProps {
   closeHandler?: () => void
 }
 
+const ICON_SIZE = {
+  height: 90,
+  width: 90
+}
+
+const AlertRoot = styled.div`
+  position: fixed;
+  z-index: 1000;
+  transform-origin: center;
+  padding: 0 1rem;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  display: flex;
+`
+
+const AlertWrapper = styled.div`
+  width: 100%;
+  margin: auto;
+  max-width: 500px;
+  height: 350px;
+  align-items: center;
+  border-radius: 5px;
+  padding: 1rem;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  box-sizing: border-box;
+`
+
+const AlertTitle = withStyles({
+  root: {
+    marginBottom: '1rem'
+  }
+})(Typography)
+
+const AlertButtonClose = withStyles({
+  root: {
+    marginTop: '2rem'
+  }
+})(GradientButton)
+
+const AlertIconSpinner = styled.div`
+  height: ${ICON_SIZE.height}px;
+  width: ${ICON_SIZE.width}px;
+  border: 5px solid white;
+  border-top: 5px solid #FF512F;
+  border-radius: 50%;
+  margin-bottom: 3rem;
+  animation: spinner 1s linear infinite;
+`
+
+const AlertIconSuccess = styled.div`
+  height: ${ICON_SIZE.height}px;
+  width: ${ICON_SIZE.width/2}px;
+  margin-bottom: 2rem;
+  transform: rotate(45deg);
+  border-right: 5px solid #FF512F;
+  border-bottom: 5px solid #FF512F;
+`
+
+const AlertIconError = styled.div`
+  margin-bottom: 2rem;
+  height: ${ICON_SIZE.height}px;
+  width: ${ICON_SIZE.width}px;
+  position: relative;
+  transform: rotate(45deg);
+  transform-origin: center;
+  & div {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+  };
+  & div:first-child {
+    width: 100%;
+    height: 5px;
+    background-color: #FF512F;
+    transform: translate(-50%, -50%);
+  };
+  & div:nth-child(2) {
+    width: 100%;
+    height: 5px;
+    background-color: #FF512F;
+    transform: translate(-50%, -50%) rotate(90deg);
+  }
+`
+
+const AlertCloseZone = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  height: 100%;
+  z-index: -1
+`
+
 const AlertContent = (props: AlertContentProps): ReactElement => {
   return (
     <>
       {props.icon}
-      <StyledAlertTitle variant="h3">{props.title}</StyledAlertTitle>
+      <AlertTitle variant="h3">{props.title}</AlertTitle>
       <Typography align="center">{props.message}</Typography>
       {props.closeHandler && (
-        <StyledAlertButtonClose onClick={props.closeHandler}>
+        <AlertButtonClose onClick={props.closeHandler}>
           <WhiteTypography>Close</WhiteTypography>
-        </StyledAlertButtonClose>
+        </AlertButtonClose>
       )}
     </>
   )
@@ -58,18 +148,18 @@ export default function Alert(props: Props): ReactElement {
         timeout={500}
         unmountOnExit
       >
-        <StyledAlertRoot>
-          <StyledAlert>
+        <AlertRoot>
+          <AlertWrapper>
             {props.variant === "wait" && (
               <AlertContent
-                icon={<StyledAlertIconSpinner />}
+                icon={<AlertIconSpinner />}
                 title="Melakukan request..."
                 message={props.message || "Silahkan tunggu beberapa saat"}
               />
             )}
             {props.variant === "success" && (
               <AlertContent
-                icon={<StyledAlertIconSuccess />}
+                icon={<AlertIconSuccess />}
                 title="Pendaftaran Berhasil"
                 message={props.message || "Selamat ! kami akan mengirim email sesaat lagi"}
                 closeHandler={closeAlertHandler}
@@ -78,19 +168,19 @@ export default function Alert(props: Props): ReactElement {
             {props.variant === "error" && (
               <AlertContent
                 icon={
-                  <StyledAlertIconError>
+                  <AlertIconError>
                     <div></div>
                     <div></div>
-                  </StyledAlertIconError>
+                  </AlertIconError>
                 }
                 title="Pendaftaran Gagal"
                 message={props.message || "Mohon maaf terjadi kesalahan, silahkan coba beberapa saat lagi"}
                 closeHandler={closeAlertHandler}
               />
             )}
-          </StyledAlert>
-          <StyledAlertCloseZone onClick={closeAlertHandler}/>
-        </StyledAlertRoot>
+          </AlertWrapper>
+          <AlertCloseZone onClick={closeAlertHandler}/>
+        </AlertRoot>
       </CSSTransition>
       <CSSTransition
         in={props.isShow}
