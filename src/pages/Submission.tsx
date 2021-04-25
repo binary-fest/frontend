88ce179b32b93b064b34df800caa0ec5d5838e55
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-04-25 13:26:34 +0700

    fix(submission): remove unused variable

--- src/pages/Submission.tsx
+++ src/pages/Submission.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react'
 import {
   AbsoluteFormHelperText,
   GradientButton,
-  GradientTypography,
   StaticPageContentStyled,
   WhiteInput,
   WhiteInputLabel,
